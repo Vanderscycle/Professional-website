fccdfe2cf74ea991313e38331d275fe553cbac49
Henri Vandersleyen <hvandersleyen@duck.com>
2022-02-28 13:41:58 -0800

    updated packages and modified the index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,8 +14,9 @@
 	<h1 class="relative text-3xl dark:text-terminalDarkText text-terminalLightText">
 		Henri Vandersleyen
 	</h1>
-
-	Linux Loving, Terminal centric, Motivated Software devloper
+	<span class="dark:text-terminalDarkText text-terminalLightText">
+		Linux Loving, Terminal centric, Motivated Software devloper</span
+	>
 	<div class="grid">
 		<div class="w-5/6 ">
 			<Terminal terminalData={terminalMsg} terminalName="Welcome" terminalCommand="greet" />
