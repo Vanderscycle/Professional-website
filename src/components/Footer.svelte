a4e8c2aa1f5de064a021277e25f28de96f6189c1
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-01 13:30:12 -0800

    better copyright label

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-  // your script goes here
+	// your script goes here
 </script>
 
 <template>
-  <div class="dark:text-terminalDarkBlack dark:bg-terminalDarkGui text-terminalLightBg bg-terminalLightGui
-    fixed inset-x-0 bottom-0 text-center ">
-    Copyright {new Date().getFullYear()}
-  </div>
+	<div
+		class="dark:text-terminalDarkBlack dark:bg-terminalDarkGui text-terminalLightBg bg-terminalLightGui
+    fixed inset-x-0 bottom-0 text-center "
+	>
+		<small>&copy;Copyright {new Date().getFullYear()} Henri Vandersleyen </small>
+	</div>
 </template>
 
 <style lang="postcss">
-  /* your styles go here */
+	/* your styles go here */
 </style>
-
