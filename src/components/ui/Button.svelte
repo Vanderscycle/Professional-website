1390d6c6ddbb2ca4302c61e85387d2f25140a94f
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-03 15:03:09 -0800

    added a button, found what I want to do with my simple API

---
+++ src/components/ui/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+export let callbackFn: () => void;
+
+</script>
+
+<template>
+  <button 
+  on:click={callbackFn}
+  class="inline-flex items-center justify-evenly p-2
+    shadow-sm rounded-md
+    hover:bg-terminalDarkWhite hover:text-terminalDarkBg 
+    border-double border-4 border-white"
+  >
+    <slot />
+</button>
+  </template>
+
+<style lang="postcss"></style>
