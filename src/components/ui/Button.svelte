0f3df313aabd4d7aca2fc3a7024b68a3e3ab0239
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-03 15:55:56 -0800

    found a very elegant solution to my offer problem (using a ternary operator and string formatter)

--- src/components/ui/Button.svelte
+++ src/components/ui/Button.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
-export let callbackFn: () => void;
+	export let callbackFn: () => void;
+	import { uiState } from "$stores/ui";
+	let { darkMode } = uiState;
 
+	let hoverState: string = "";
+	$: hoverState = $darkMode
+		? "hover:bg-terminalLightWhite hover:text-terminalLightBg"
+		: "hover:bg-terminalDarkWhite hover:text-terminalDarkBg";
 </script>
 
 <template>
-  <button 
-  on:click={callbackFn}
-  class="inline-flex items-center justify-evenly m-2 px-1
-    shadow-sm rounded-md hover:bg-terminalDarkWhite hover:text-terminalDarkBg border-4 border-white"
-  >
-    <slot />
-</button>
-  </template>
+	<button
+		on:click={callbackFn}
+		class={`inline-flex items-center justify-evenly m-2 px-1 shadow-sm rounded-md border-2 underline decoration-1 border-white ${hoverState}`}
+	>
+		<slot />
+	</button>
+</template>
 
 <style lang="postcss"></style>
