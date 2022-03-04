4b052f2341136175174194d8ff9b90ec84eccfe9
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-04 14:13:14 -0800

    named slots with fallbacks are amazing

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -3,9 +3,31 @@
 
 <template>
 	<div class="border-2 border-white flex grow p-2 rounded-xl">
-		<slot name="title">
-			<span class="underline">Missing Title</span>
+		<slot name="img">
+			<span class="underline text-red-500 rounded-xl bg-yellow-400 p-2 m-2 border-2 border-blue-400"
+				>Missing Image
+			</span>
 		</slot>
+		<div class="text-center flex flex-col justify-center items-center">
+			<slot name="title">
+				<span
+					class="underline text-red-500 rounded-xl bg-yellow-400 p-2 m-2 border-2 border-blue-400"
+					>Missing Title
+				</span>
+			</slot>
+			<slot name="Description">
+				<span
+					class="underline text-red-500 rounded-xl bg-yellow-400 p-2 m-2 border-2 border-blue-400"
+					>Missing Description</span
+				>
+			</slot>
+			<slot name="Badges">
+				<span
+					class="underline text-red-500 rounded-xl bg-yellow-400 p-2 m-2 border-2 border-blue-400"
+					>Missing Badges</span
+				>
+			</slot>
+		</div>
 	</div>
 </template>
 
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -11,8 +11,11 @@
 		class="text-center flex flex-col justify-center items-center dark:text-terminalDarkText text-terminalLightText"
 	>
 		<h1 class="relative text-3xl  py-4 underline">Projects</h1>
-		<div class="">
+		<div class="py-2">
 			<ProjectCard><span slot="title">Dotfiles</span></ProjectCard>
+		</div>
+		<div class=" py-2">
+			<ProjectCard><span slot="title">Profeesional Website</span></ProjectCard>
 		</div>
 	</div>
 </template>
