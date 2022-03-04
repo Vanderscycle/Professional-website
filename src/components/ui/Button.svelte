8a48cd09d75e582bbf3a5b6beac0307df145c1e0
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-03 16:10:49 -0800

    added some interactivity to the website

--- src/components/ui/Button.svelte
+++ src/components/ui/Button.svelte
@@ -5,14 +5,14 @@
 
 	let hoverState: string = "";
 	$: hoverState = $darkMode
-		? "hover:bg-terminalLightWhite hover:text-terminalLightBg"
-		: "hover:bg-terminalDarkWhite hover:text-terminalDarkBg";
+		? "hover:text-terminalLightWhite hover:bg-terminalLightBg hover:decoration-terminalLightRed"
+		: "hover:text-terminalDarkWhite hover:bg-terminalDarkBg hover:decoration-terminalDarkRed";
 </script>
 
 <template>
 	<button
 		on:click={callbackFn}
-		class={`inline-flex items-center justify-evenly m-2 px-1 shadow-sm rounded-md border-2 underline decoration-1 border-white ${hoverState}`}
+		class={`inline-flex items-center justify-evenly m-2 px-1 shadow-sm rounded-md border-2 underline decoration-1 border-white ${hoverState} transition ease-in-out delay-100 hover:-translate-y-1`}
 	>
 		<slot />
 	</button>
