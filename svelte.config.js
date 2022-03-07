f50e30b5488d16c7bf7b60b09ab28caa9db7f97f
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-06 16:34:51 -0800

    small touches

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
-import { resolve } from "path";
 import path from "path";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
