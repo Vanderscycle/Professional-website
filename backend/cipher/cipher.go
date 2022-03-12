0e1a061798bcb388942314915dfbe6884a0c4d4d
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-12 10:09:08 -0800

    conversion to go backend started

---
+++ backend/cipher/cipher.go
@@ -0,0 +1,53 @@
+package cipher
+
+import (
+	"backend/database"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type BlogPost struct {
+	gorm.Model
+	Title    string `json:"title"`
+	Author   string `json:"author`
+	MainText string `json:"mainText"`
+}
+
+func Get(c *fiber.Ctx) error {
+	db := database.DBConn
+	var entry []BlogPost
+	db.Find(&entry)
+	return c.JSON(entry)
+}
+
+func Get(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DBConn
+	var entry BlogPost
+	db.Find(&entry, id)
+	return c.JSON(entry)
+}
+
+func New(c *fiber.Ctx) error {
+	db := database.DBConn
+	entry := new(BlogPost)
+	if err := c.BodyParser(entry); err != nil {
+		return c.Status(503).Send([]byte("error"))
+	}
+	db.Create(&entry)
+	return c.JSON(entry)
+}
+
+func Delete(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DBConn
+	var entry BlogPost
+	db.First(&entry, id)
+	if entry.Title == "" {
+		errMsg := []byte("No record matches the id")
+		return c.Status(500).Send(errMsg)
+	}
+	db.Delete(&entry) // TODO: how to concat to byte
+	msg := []byte("post deleted successfully")
+	return c.Send(msg)
+}
