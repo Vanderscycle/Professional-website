9c2906c9d35c3c2a7460e93b0891cd8088135e42
vanderscycle <henri-vandersleyen@protonmail.com>
2024-03-24 19:49:24 -0700

    updates + dark mode works

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -14,11 +14,11 @@
 	<title>Henri Vandersleyen</title>
 </svelte:head>
 
-<div class='class={$darkMode === true ? "dark h-full" : "h-full"}'>
+<div class='{$darkMode === true ? "dark h-full" : "h-full"}'>
 	<nav class="dark:bg-darkGui bg-Gui">
 		<Navbar />
 	</nav>
-	<main class="">
+	<main class="dark:darkBg Bg">
 		<Modal>
 			<slot />
 		</Modal>
